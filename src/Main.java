f782910512fb10488a205a311beae986a4cc2c72
r4venj <r4venj@protonmail.com>
2024-06-11 19:49:32 -0300

    Projeto JAVA

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,49 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
+        // String nome = "Joey";
+        String nome;
+        nome = "Joey Melo";
+        System.out.println("Olá "+ nome);
+        int a = 1;
+        int b;
+        b = 2;
+        System.out.println("");
+        System.out.println("1)");
+        int somaAB = a + b;
+        int subtracaoAB = a - b;
+        int multiplicacaoAB = a * b;
+        float divisaoAB = a / b;
+        System.out.println(somaAB);
+        System.out.println(subtracaoAB);
+        System.out.println(multiplicacaoAB);
+        System.out.println(divisaoAB);
+        System.out.println("");
+        System.out.println("2)");
+        float c = 1;
+        float d;
+        d = 2;
+        float somaCD = c + d;
+        float subtracaoCD = c - d;
+        float multiplicacaoCD = c * d;
+        float divisaoCD = c / d;
+        System.out.println(somaCD);
+        System.out.println(subtracaoCD);
+        System.out.println(multiplicacaoCD);
+        System.out.println(divisaoCD);
+        System.out.println("");
+        System.out.println("3)");
+        int e = 1;
+        int f;
+        f = 2;
+        int somaEF = e + f;
+        int subtracaoEF = e - f;
+        int multiplicacaoEF = e * f;
+        float divisaoEF = (float) e / f;
+        System.out.println(somaEF);
+        System.out.println(subtracaoEF);
+        System.out.println(multiplicacaoEF);
+        System.out.println(divisaoEF);
     }
 }
